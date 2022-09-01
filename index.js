e5331cd49a0f126f779e870afd5cb56a3b52d8f3
kierah mcgill <kierahalexis7@gmail.com>
2022-09-01 11:25:12 -0400

    commit tree c038bea7676684b5b196185ba5c0cfc5d80b6be5

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(5, 10, 15, 20);
+console.log(max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
